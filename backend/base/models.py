ba1d05942b0e43de617502359712ff5505669ce4
Nut Milk <85531672+big-cock-benny@users.noreply.github.com>
2021-07-03 22:36:09 +0100

    Created and related all our models

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
+# Importing Django's predefined user model
+from django.contrib.auth.models import User
+from django.db.models.fields import DateTimeField
+
 # Create your models here.
+class Product(models.Model):
+
+    # Here we let the user be linked one to many with Product and on delete this field just gets set to null
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # image = 
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    num_reviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    count_in_stock = models.IntegerField(null=True, blank=True, default=0)
+
+    # Here we automatically create the datetime at time of creation
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Automatically setting the product ID as the primary key
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        '''
+        Django uses this standard dunder str method to represent the
+        object in the model. doing this allows us to make the self.name
+        value the name that is printed in our model database
+        '''
+        return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.rating)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    payment_method = models.CharField(max_length=200, null=True, blank=True)
+    tax_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    shipping_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
+    paid_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    is_delivered = models.BooleanField(default=False)
+    delivered_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.created_at)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postcode = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shipping_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.address)
