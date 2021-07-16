84914f02c8626b9cbdff80fd2ea9af7d00de07c8
Nut Milk <85531672+big-cock-benny@users.noreply.github.com>
2021-07-16 02:03:12 +0100

    Setup our redux state for login in the frontend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import {
   productListReducer,
   productDetialsReducer,
 } from "./reducers/productReducers";
+import { userLoginReducer } from "./reducers/userReducer";
 import { cartReducer } from "./reducers/cartReducers";
 import thunk from "redux-thunk";
 
@@ -12,14 +13,20 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetialsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
-const cartItemsFromStorage = localStorage.getItem('cartItems')
-  ? JSON.parse(localStorage.getItem('cartItems'))
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 const middleware = [thunk];
 const store = createStore(
