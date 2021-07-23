8bcf9e1718a4d801c03cba1ff6282901b44490da
Kamal Sacranie <kamalsjunkmail@gmail.com>
2021-07-23 15:11:17 +0100

    Create checkout steps component
    Added a checkout steps component to our checkout screen which shows how
    far along we are in the checkout process

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,7 +2,9 @@ import { Form, Button } from 'react-bootstrap'
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
+
 
 function ShippingScreen({ history }) {
 
@@ -31,6 +33,7 @@ function ShippingScreen({ history }) {
     return (
 
         <FormContainer>
+            <CheckoutSteps step1 step2/>
             <h1>Shipping</h1>
            <Form onSubmit={submitHandler}>
                
