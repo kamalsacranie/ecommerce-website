e1bc63c1a7610b7e4f6b33f0959fa362b62541c7
Kamal Sacranie <kamalsjunkmail@gmail.com>
2021-07-28 13:46:03 +0100

    Implanted profile order details
    Made state for all a user's orders which shows the status and shipping
    etc.

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Form, Button, Row, Col, Table } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 
 import Loader from '../components/Loader'
@@ -29,6 +29,9 @@ function ProfileScreen({ history }) {
 
     const userUpdateProfile = useSelector(state => state.userUpdateProfile)
     const { success } = userUpdateProfile
+
+    const orderListMy = useSelector(state => state.orderListMy)
+    const { loading: loadingOrders, error: errorOrders, orders } = orderListMy
     
     useEffect(() => {
         if(!userInfo) {
@@ -124,6 +127,46 @@ function ProfileScreen({ history }) {
             </Col>
             <Col md={9}>
                 <h2>My Orders</h2>
+                {loadingOrders ? (
+                    <Loader />
+                ) : errorOrders ? (
+                    <Message variant='danger'>{errorOrders}</Message>
+                ) : (
+                    <Table striped responsive className="table-sm">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Date</th>
+                                <th>Total</th>
+                                <th 
+                                  style={{textAlign: 'center'}}
+                                >Paid</th>
+                                <th>Delivered</th>
+                                <th></th>
+                            </tr>
+                        </thead> 
+
+                        <tbody>
+                            {orders.map(order => (
+                                <tr key={order._id}>
+                                    <td>{order._id}</td>
+                                    <td>{order.created_at.substring(0, 10)}</td>
+                                    <td>${order.total_price}</td>
+                                    <td 
+                                      style={{textAlign: 'center'}}
+                                    >{order.is_paid ? order.paid_at.substring(0, 10) : (
+                                        <i className='fas fa-times' style={{color: 'red'}}></i>
+                                    )}</td>
+                                    <td>
+                                        <LinkContainer to={`/order/${order._id}`}>
+                                            <Button className='btn-sm'>Details</Button>
+                                        </LinkContainer>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                )}
             </Col>
         </Row>
     )
