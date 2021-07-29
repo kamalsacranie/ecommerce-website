2c29cea897423b62eeaefaae26d34d5ce42c8108
Kamal Sacranie <kamalsjunkmail@gmail.com>
2021-07-30 00:59:43 +0100

    Create backend for deleting user
    Created our path in product_urls to launch our delete_product view which
    sends a DELETE request to our database to delete a product

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -4,4 +4,6 @@ from base.views import product_views as views
 urlpatterns = [
     path('', views.get_products, name='products'),
     path('<str:pk>/', views.get_product, name='product'), # url for the specific product which passes thorugh the pk to the function
+
+    path('delete/<str:pk>/', views.delete_product, name='product-delete')
 ]
