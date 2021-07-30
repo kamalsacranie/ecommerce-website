7f90d06be65564b3f8bc6899276ef5fbde0234d5
Kamal Sacranie <kamalsjunkmail@gmail.com>
2021-07-30 01:10:34 +0100

    Added our deleteProduct acitons in prodActions
    We also added it to our ProductListScreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,9 +3,14 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
 } from "../constants/productConstants";
 
 // instead of making our API call from our home screen we
@@ -45,6 +50,44 @@ export const listProductDetails = (id) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+
+  try {
+    dispatch({
+      type: PRODUCT_DELETE_REQUEST,
+    });
+
+    const { 
+      userLogin: { userInfo }
+     } = getState()
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}` // grabbing our token from userInfo and allowing us to authorise
+      },
+    };
+
+    const { data } = await axios.delete(
+      `/api/products/delete/${id}`,
+      config,
+    );
+
+    dispatch({
+      type: PRODUCT_DELETE_SUCCESS,
+    });
+
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DELETE_FAIL,
       payload:
         error.response && error.response.data.detail
           ? error.response.data.detail
